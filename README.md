a83edd64738e40c2229efa446e1e1a2d02e05778
shimehituzi <shimehituzi@gmail.com>
2024-01-02 02:37:21 +0900

    init README.md and install.sh

---
+++ README.md
@@ -0,0 +1,29 @@
+# My dotfiles
+
+**dotfiles for me**
+
+## Install Command
+
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/shimehituzi/.dotfiles/master/install.sh)"
+```
+
+## requirement
+
+fonts: [UDEV Gothic](https://github.com/yuru7/udev-gothic)
+
+## manual setteings
+
+### iTerm Load Setting file
+
+iTerm -> Preferences -> Preferences
+
+Load preferences from a custom folder or URL: `/Users/[username]/.config/iterm2`
+
+## Note: Update Submodule
+
+in `~/.dotfiles`
+
+```bash
+git submodule update --remote
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
+[[ -e ~/.dotfiles ]] || git clone git@github.com:shimehituzi/.dotfiles.git ~/.dotfiles
+pushd ~/.dotfiles
 
+git submodule init
+git submodule update
+for file in `ls -a`; do
+  [ $file = '.' ] && continue
+  [ $file = '..' ] && continue
+  [ $file = '.git' ] && continue
+  [ $file = '.gitmodules' ] && continue
+  [ $file = '.gitignore' ] && continue
+  [ $file = 'README.md' ] && continue
+  [ $file = 'install.sh' ] && continue
+  ln -snf ~/.dotfiles/$file ~/
+done
+
+pushd ~/
+
+which -s brew
+if [[ $? != 0 ]] ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+brew bundle --no-lock
+
+popd
+
+popd

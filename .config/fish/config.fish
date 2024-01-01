1839c4a88b9c866421d9070f02023ee4d25f5d58
shimehituzi <shimehituzi@gmail.com>
2024-01-01 22:02:33 +0900

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+set -U fish_greeting
+
+set -gx EDITOR nvim
+
+if status is-interactive
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+function bottom_prompt --on-event fish_prompt
+  tput cup $LINES 0
+end
+function ctrl_l
+  clear
+  tput cup $LINES 0
+  commandline -f repaint
+end
+bind \cl ctrl_l
+
+function starship_transient_prompt_func
+ starship module character
+end
+starship init fish | source
+enable_transience
+
+rtx activate fish | source
---
+++ Brewfile
@@ -0,0 +1,13 @@
+tap "homebrew/bundle"
+brew "fd"
+brew "fish"
+brew "gh"
+brew "git"
+brew "lazygit"
+brew "lsd"
+brew "neovim", args: ["HEAD"]
+brew "ripgrep"
+brew "rtx"
+brew "starship"
+brew "zellij"
+cask "iterm2"

85943fa8ac17aa532f0e09df8501b861ad43a48c
shimehituzi <shimehituzi@shimehituzinoMac-mini.local>
2024-01-02 11:49:30 +0900

    change install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,57 @@
 #!/bin/sh
 
-[[ -e ~/.dotfiles ]] || git clone git@github.com:shimehituzi/.dotfiles.git ~/.dotfiles
-pushd ~/.dotfiles
+dotfiles_path="$HOME/.dotfiles"
 
-git submodule init
-git submodule update
-for file in `ls -a`; do
-  [ $file = '.' ] && continue
-  [ $file = '..' ] && continue
-  [ $file = '.git' ] && continue
-  [ $file = '.gitmodules' ] && continue
-  [ $file = '.gitignore' ] && continue
-  [ $file = 'README.md' ] && continue
-  [ $file = 'install.sh' ] && continue
-  ln -snf ~/.dotfiles/$file ~/
-done
+install_dotfiles() {
+  [[ -e ~/.dotfiles ]] || git clone git@github.com:shimehituzi/.dotfiles.git ~/.dotfiles
+}
 
-pushd ~/
+install_gitsubmodule() {
+  git submodule init
+  git submodule update
+}
 
-which -s brew
-if [[ $? != 0 ]] ; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-brew bundle --no-lock
+install_pkg_manager() {
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+}
 
-popd
+install_packages() {
+  brew bundle --no-lock
+}
 
-popd
+generate_symlink() {
+  for file in `ls -a ${dotfiles_path}`; do
+    [ $file = '.' ] && continue
+    [ $file = '..' ] && continue
+    [ $file = '.git' ] && continue
+    [ $file = '.gitmodules' ] && continue
+    [ $file = '.gitignore' ] && continue
+    [ $file = 'README.md' ] && continue
+    [ $file = 'install.sh' ] && continue
+    ln -snf ${dotfiles_path}/${file} $HOME
+  done
+}
+
+
+main() {
+
+  install_dotfiles()
+
+  pushd ${dotfiles_path}
+
+  install_gitsubmodule()
+
+  generate_symlink()
+
+  pushd $HOME
+
+  install_pkg_manager()
+
+  install_packages()
+
+}
+
+main "$@"

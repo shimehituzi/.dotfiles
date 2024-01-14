9f8f5c9249936ea068769a5a5f326e840dce1543
shimehituzi <shimehituzi@gmail.com>
2024-01-14 13:15:04 +0900

    update abbr

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -1,2 +1,6 @@
 abbr --add enja trans -b -s en -t ja
 abbr --add jaen trans -b -s ja -t en
+abbr --add fishdir cd ~/.config/fish
+abbr --add nvimdir cd ~/.config/nvim
+abbr --add dotdir cd ~/.dotfiles
+abbr --add gs git status
